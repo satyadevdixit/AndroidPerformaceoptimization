9209ef94889758aecf9575c5dbe3d4f3e214ff33
yogesh <yogeshvrma007@gmail.com>
2022-11-13 18:56:53 +0530

    Hello world

--- app/src/main/java/com/example/androidperformanceoptimization/MainActivity.kt
+++ app/src/main/java/com/example/androidperformanceoptimization/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Hello World")
+        println("Hello World 2")
     }
 }

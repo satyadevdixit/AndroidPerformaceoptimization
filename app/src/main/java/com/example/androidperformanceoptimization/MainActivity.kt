ea674670dbc7a1a0f521cb6075628205b6d84c93
satyadevdixit <devdixit1996@gmail.com>
2022-11-15 17:17:09 +0530

    Add lifecyle methods in Mainactivity

--- app/src/main/java/com/example/androidperformanceoptimization/MainActivity.kt
+++ app/src/main/java/com/example/androidperformanceoptimization/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.androidperformanceoptimization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("MainActivity","onCreate")
         setContentView(R.layout.activity_main)
-        println("Hello World")
-        println("Hello World 2")
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("MainActivity","onPause")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("MainActivity","onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("MainActivity","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("MainActivity","onDestroy")
+    }
+
+
+
 }
